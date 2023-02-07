76fced82d1393c06403ac44af887d6e59bd73b03
Chandra Prakash S <scp.thedevil@gmail.com>
2023-02-07 17:47:07 +0000

    Volume of Sphere
    Signed-off-by: Chandra Prakash S <scp.thedevil@gmail.com>

---
+++ Session-1/volumeOfSphere.py
@@ -0,0 +1,10 @@
+"""
+ Laasya bought a new volleyball in the sports shop. It looks like a medium size. 
+ She somehow found the radius of the sphere.
+ But she would like to know the volume of that ball. 
+ Can you help him in finding the Volume of the ball? 
+"""
+r = float(input(''))
+pi = 3.14
+volume = (4.0/3.0)*pi*r**3
+print(volume)
