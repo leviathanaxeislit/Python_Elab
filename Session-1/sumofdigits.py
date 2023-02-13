d5eca65483bf3c8a310c37eec066b1cc2d2168f5
Chandra Prakash S <scp.thedevil@gmail.com>
2023-02-13 17:26:42 +0000

    Add Questions
    Signed-off-by: Chandra Prakash S <scp.thedevil@gmail.com>

--- Session-1/sumofdigits.py
+++ Session-1/sumofdigits.py
@@ -1,3 +1,8 @@
+"""
+215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+What is the sum of the digits of the number 2n ?
+"""
+
 def calculate(n, power):
     return sum([int(i) for i in str(pow(n, power))])
 n = 2
