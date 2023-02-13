4d559bf72190278f95d5c02eabf2cd4c9e298851
Chandra Prakash S <scp.thedevil@gmail.com>
2023-02-13 16:13:46 +0000

    Rajadhani Express
    Signed-off-by: Chandra Prakash S <scp.thedevil@gmail.com>

---
+++ Session-1/rajadhaniexpress.py
@@ -0,0 +1,11 @@
+"""
+Binita was travelling from Chennai to Delhi in Rajdhani Express. 
+The train have arrived at the destination later than the estimated time. 
+So, Binita wants to know the total number of hours and minutes the train was delayed. 
+Can you help Binita in finding the exact hour and time Rajdhani Express was delay on the day of Binita's journey?
+"""
+
+tot = int(input(''))
+hrs = int(tot/60)
+mins = int(tot % 60)
+print(hrs, 'Hours and', mins, 'Minutes')
