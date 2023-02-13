d982783530894d1d2065de84af9dd8747ce5ff84
Chandra Prakash S <scp.thedevil@gmail.com>
2023-02-13 15:32:27 +0000

    First Name Last Name
    Signed-off-by: Chandra Prakash S <scp.thedevil@gmail.com>

---
+++ Session-1/firstnamelastname.py
@@ -0,0 +1,7 @@
+# Question description 
+# Timothy Boon having the first name Timothy and Last name is Boon. 
+# Can you help him to make a program to display his name as Boon Timothy without using swap function. 
+
+fname = input()
+lname = input()
+print("Hello "+ lname + " "+ fname)
